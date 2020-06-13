e0462926cf510fb051924a80965578326cde182e
Austin Pagotto <austinpagotto@gmail.com>
2020-06-13 10:30:14 -0400

    Stripe billing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieSession = require('cookie-session')
-const passport = require('passport')
-const keys = require("./config/keys")
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+const bodyParser = require('body-parser');
 require("./models/user");
 require("./services/passport");
 
@@ -11,18 +12,21 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(
-    cookieSession({
-        maxAge:30 *24 * 60 * 60 *1000,
-        keys:[keys.cookieKey]
-    })
-)
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // this calls the routes
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 // This figures out what port to listen to Heroku will inject this environment variable or for development we use 5000
 const PORT = process.env.PORT || 5000;
--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const { Schema } = mongoose;
 // create schema for collection
 const userSchema = new Schema({
   googleId: String,
+  credits: { type:Number,default:0}
 });
 
 // create model for users collection with the schema
