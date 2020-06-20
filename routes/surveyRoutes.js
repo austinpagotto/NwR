5dfe14a5d481b27c17bdb304c7f03f76893525fd
Austin Pagotto <austinpagotto@gmail.com>
2020-06-20 13:31:48 -0400

    mailer

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
-
+const Mailer = require('../services/Mailer');
 const Survey = mongoose.model("surveys");
 module.exports = (app) => {
   app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
@@ -17,5 +17,8 @@ module.exports = (app) => {
       _user: req.user.id,
       dateSent: Date.now(),
     });
+
+    // send email
+    const mailer = new Mailer(survey,template);
   });
 };
