5a2f629c10074a94533cd7f190e0ab83439b9177
Austin Pagotto <austinpagotto@gmail.com>
2020-06-23 17:11:03 -0400

    new section

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,20 @@
+const keys = require('../../config/keys')
+
 module.exports = (survey) => {
-    return '<div>'+survey.body+'</div>'
-}
+  return `
+  <html>
+  <body>
+  <div style = "text-align: center;">
+  <h3> input plz </h3>
+  <p>plz answer q</p>
+  <p>${survey.body}</p>
+  <div>
+  <a href = "${keys.redirectDomain}"/api/surveys/thanks>Yes</a>
+  </div>
+  <div>
+  <a href = "${keys.redirectDomain}"/api/surveys/thanks>No</a>
+  </div>
+  </body>
+  </html>
+  `;
+};
